4fed9559fc6391f24631d6ccc063d973600fa573
sabrina-jones <sabrina.jones87@gmail.com>
2020-07-21 12:23:39 -0700

    Done.

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,9 +1,9 @@
-class CreateBirds < ActiveRecord::Migration#[5.2]
+class CreateBirds < ActiveRecord::Migration[5.2]
   def change
     create_table :birds do |t|
       t.string :name
       t.string :species
-
+ 
       t.timestamps
     end
   end
